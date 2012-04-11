8382fb8ee027b13877c821e2865a6d318d5b15a7
ajc388 <ajc388@nau.edu>
2012-04-10 21:19:33 -0700

    Made more functions for the Mapper class.

--- src/caffeine/Mapper.java
+++ src/caffeine/Mapper.java
@@ -1,5 +1,9 @@
 package caffeine;
 
+import java.util.Stack;
+
+import lejos.nxt.LCD;
+
 /*
  * This is the mapper, which resides on caffeine
  * The mapper needs to:
@@ -22,6 +26,12 @@ public class Mapper {
 	//Robot position
 	private double xRobotPosition;
 	private double yRobotPosition;
+	private final double xStartPosition;
+	private final double yStartPosition;
+	
+	//Object Class
+	Stack superStack = new Stack();
+	
 	
 	//Constructor
 	//Builds the map object based upon the constants mapsize
@@ -39,7 +49,9 @@ public class Mapper {
     	    } 	    
     	    
     	    //At 250 250 create the starting location
-    	    map[mapSize/2][mapSize/2] = mapObject.STARTING_POINT;
+    	    xStartPosition = mapSize/2;
+    	    yStartPosition = mapSize/2;
+    	    map[(int) xStartPosition][(int) yStartPosition] = mapObject.STARTING_POINT;
 	    	
     	    //create surrounding field of 10"x12"
     	    int width = (int)25.4/mapIncrement;
@@ -94,9 +106,73 @@ public class Mapper {
     	}
     	
     	
-	public void goHome() {
+    	/*Returns a queue of suggested points to return
+    	 * back home
+    	 */
+	public void returnHome() {
+	   
+	   int xRobotPosition = (int) getXStartPosition();
+	   int yRobotoPosition = (int) getYStartPosition();
 	    
+	}
+	
+	public void exploreField(int searchIncrement) {
+	    double x = getXRobotPosition();
+	    double y = getYRobotPosition();
 	    
+	    //Establish the lower bound x and y
+	    //and the upper bound x and y
+	    int lowerBoundX = (int)(x - (searchIncrement/2));
+	    int lowerBoundY = (int)(y - (searchIncrement/2));
+	    int upperBoundX = (int)(x + (searchIncrement/2));
+	    int upperBoundY = (int)(y + (searchIncrement/2));
+	    
+	    //Base case
+	    //If the boundaries are all beyond the array
+	    //Then there is no unexplored areas to be found.
+	    if (   (lowerBoundX <= 0) && (lowerBoundY < 0)
+		 && (upperBoundX >= 500) && (upperBoundY > 500 )) {
+		//Print robots actions
+		LCD.clear();
+		LCD.drawString("No unexplored", 0, 3);
+		LCD.drawString(" areas found.", 0, 4);
+		LCD.drawString("Going home.", 0 , 5);
+		LCD.refresh();
+		
+		//Return home
+		returnHome();
+		
+	    } else {
+		//Makes sure there will be no stack
+		//overflow exceptions
+    	    	if ( lowerBoundX < 0 ) {
+    	    	    lowerBoundX = 0;
+    	    	}
+    	    	if ( lowerBoundY < 0 ) {
+    	    	    lowerBoundY = 0;
+    	    	}
+	    
+    	    	//Scan the entire section for unvisited elements in the array
+    	    	for ( int i = lowerBoundX ; i < searchIncrement && i < mapSize; i++ ) {
+    	    	    for ( int j = lowerBoundY ; j < searchIncrement && j < mapSize; j++ ) {
+		
+    	    		if ( map[i][j] == mapObject.UNVISITED_LOCATION ) {
+    	    		    //get points
+    	    		    //pass data to navigator
+    	    		    break;
+    	    		}
+		
+    	    	    }
+    	    	}
+    	    	//If nothing is found, call the function again
+    	    	//And increase the search range
+	        exploreField(searchIncrement*2);
+	    }
+	}
+	
+	private void suggestPoint() {
+	    
+	    	    
 	    
 	}
 	
@@ -111,9 +187,8 @@ public class Mapper {
 	
 	public void getNewNav() {
 		//Determines the next point that needs to be explored, tells the navigator to explore it
-	
-	    
 	}
+	
 	
 	public String toString() {	    
 	    String mapPrintout = "Map Status\n";
@@ -127,6 +202,10 @@ public class Mapper {
 	    
 	}
 	
-	public double getXRobotPosition() {  return xRobotPosition;   }
-	public double getYRobotPositionn() {  return yRobotPosition;   }
+	
+	
+	public double getXRobotPosition() { return xRobotPosition;  }
+	public double getYRobotPosition() { return yRobotPosition;  }
+	public double getXStartPosition() { return xStartPosition;  }
+	public double getYStartPosition() { return yStartPosition;  }
 }
