87e60a4d7b0dd98b085312c6026f4f5efc282e10
Steffen Olson <so72@nau.edu>
2012-04-12 00:49:22 -0700

    Added ObjectDetector

--- src/caffeine/ObjectDetector.java
+++ src/caffeine/ObjectDetector.java
@@ -1,21 +1,31 @@
+import lejos.nxt.UltrasonicSensor;
 
 /*
  * Object detector residing on caffeine2
  */
-public class ObjectDetector {
-	public ObjectDetector(){
+public class ObjectDetector implements Runnable{
+	//Add arraylist later
+	private boolean running;
+	Navigator nav;
+	UltrasonicSensor US1, US2, US3; //Front, Left, and Right
+	public ObjectDetector(UltrasonicSensor US1, UltrasonicSensor US2, UltrasonicSensor US3, Navigator nav){
 		//Initialize list of found blue balls to ignore
 		//Initialize the three sensors, start a thread that queries them repeatedly
+		this.US1 = US1;
+		this.US2 = US2;
+		this.US3 = US3;
+		this.nav = nav;
+		running = true;
 	}
 	public void stopScanning(){
+		running = false;
 		//When red ball is found, don't need to find it any more!
 	}
-	
-	
-	//Make a thread here to scan and notify
-	
-	
+
+
+
 	public void notifyBallColor(boolean wasRed){
+
 		//wasRed tells whether the ball that was navigated to was red or not.
 		//If blue, add to list of blue balls to ignore for detection purposes
 	}
@@ -27,5 +37,43 @@ public class ObjectDetector {
 	}
 	public void notifyCageIsBackDown(){
 		//Cage is down, we have captured ball, tell mapper to go home
+	}
+
+	public void run() {
+		while (running){
+			int	d1 = US1.getDistance();
+			int d2 = US2.getDistance();
+			int d3 = US3.getDistance();
+			if (d1<50){
+				notifyFront(d1);
+			}
+			if (d2<50){
+				notifyRight(d2);
+			}
+			if (d3<50){
+				notifyLeft(d3);
+			}
+		}
+	}
+
+	private void notifyFront (int dist){
+		
+	}
+	private void notifyRight (int dist){
+		stopScanning();
+		nav.toggleListenToMapper(false);
+		Thread.sleep(1000);// TODO: Make this more precise later!
+		int d1 = US2.getDistance();
+		nav.travel(10);
+		int d2 = US2.getDistance();
+		int deltaX = (d1*d1+100-(d2*d2))/20;
+		int deltaY = (int)(Math.sqrt((double)(d1*d1-deltaX*deltaX)));
+		int Xn = nav.getX() + 9 + deltaX;
+		int Yn = nav.getY() - 4.5 + deltaY;
+		nav.navigateTo(Xn, Yn);
+		
+	}
+	private void notifyLeft (int dist){
+		
 	}
 }
