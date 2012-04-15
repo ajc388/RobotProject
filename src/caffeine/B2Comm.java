def296d4a14832086927a28a1cf70d1e0da17dae
Leigh McGill <lm547@nau.edu>
2012-04-15 12:34:06 -0700

    Redesign beginning

--- src/caffeine/B2Comm.java
+++ src/caffeine/B2Comm.java
@@ -1,5 +1,3 @@
-package caffeine;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
@@ -7,8 +5,6 @@ import javax.bluetooth.RemoteDevice;
 
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
-import lejos.nxt.SensorPort;
-import lejos.nxt.TouchSensor;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
 
@@ -16,16 +12,11 @@ import lejos.nxt.comm.Bluetooth;
  * This is the Communicator to go on caffeine2
  * this will receive communication initiation
  */
-public class B2Comm implements Runnable{
-	private Navigator nav;
-	private DataInputStream dis;
+public class B2Comm {
 	private DataOutputStream dos;
-	private boolean connected;
-	private ObjectDetector od;
 
 	public B2Comm(){
 		//Constructor
-		connected = false;
 		String name = "caffeine";
 		LCD.drawString("Connecting...", 0, 0);
 		RemoteDevice btrd = Bluetooth.getKnownDevice(name);
@@ -47,70 +38,17 @@ public class B2Comm implements Runnable{
 
 		LCD.clear();
 		LCD.drawString("Connected", 0, 0);
-		connected = true;
-		dis = btc.openDataInputStream();
 		dos = btc.openDataOutputStream();
-		run();
 	}
 
-	public void setNavigator(Navigator nav){
-		this.nav = nav;
-	}
 
-	public void setObjectDetector(ObjectDetector od){
-		this.od = od;
-	}
-
-	public void notifyInPosition(){
-		//Notifies that it is in position for scanning a ball
-		//Code 7
+	public void sendReading(byte US, byte dist){
+		//Send which US it's from and the distance
 		try {
-			dos.writeByte(7);
-		} catch (IOException e) {
-		}
-	}
-	public void notifyReadyToLower(){
-		//Notifies that it is in position to lower the cage for capturing
-		//Code 8
-		try {
-			dos.writeByte(8);
+			dos.writeByte(US);
+			dos.writeByte(dist);
 		} catch (IOException e) {
 		}
 
-	}
-	public void requestNewNav(){
-		//Asks the mapper to send a new navigation command
-		//Code 9
-		try {
-			dos.writeByte(9);
-		} catch (IOException e) {
-		}
-	}
-	public void sendCoordinates(){
-		//Sends coordinates to the other brick
-		//Code 10 followed by 2 longs
-		try{
-			dos.writeByte(10);
-			dos.writeLong(nav.getX());
-			dos.writeLong(nav.getY());
-		} catch (IOException e) {
-		}
-	}
-	public void run() {
-		while(connected){
-			try {
-				byte b = dis.readByte();
-				switch (b) {
-				case 1: nav.emergencyStop(); break;
-				case 2: od.notifyBallColor(true); break;
-				case 3: od.notifyBallColor(false); break; //Check what else needs to happen here
-				case 4: nav.navigateTo(dis.readLong(), dis.readLong()); break;
-				case 5: nav.travel(6); break;
-				case 6: sendCoordinates(); break;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-		}
 	}
 }
