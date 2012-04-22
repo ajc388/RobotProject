953d88ff93d1ac8d368928e18b32efb409d77dca
Leigh McGill <lm547@nau.edu>
2012-04-22 13:16:15 -0700

    arghhhhh

--- src/caffeine/ColorSensorDetector.java
+++ src/caffeine/ColorSensorDetector.java
@@ -1,6 +1,6 @@
 import lejos.nxt.LCD;
-import lejos.nxt.ColorSensor.Color;
 import lejos.nxt.addon.ColorSensorHT;
+import lejos.robotics.Color;
 
 public class ColorSensorDetector {
  
@@ -11,7 +11,7 @@ public class ColorSensorDetector {
     public byte isItRed()
     {
     	//Scans ahead of it, returns true if detecting a red ball, otherwise false
-    	Color c = (Color) color.getColor();
+    	Color c = color.getColor();
     	LCD.drawString("R:" + c.getRed() + " B:" + c.getBlue(), 0, 2);
     	if(c.getRed() > c.getBlue()) { return 1;  }
     	else { return 0; }
